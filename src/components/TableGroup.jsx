f66493f12def14b8e4ab60ebb3f24e1a42924915
Hanhtunaung97 <littlehanhtunaung97>
2024-05-01 21:43:13 +0630

    meta-tag added

--- src/components/TableGroup.jsx
+++ src/components/TableGroup.jsx
@@ -30,7 +30,7 @@ const TableGroup = () => {
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              className="w-4 h-4 text-violet-500 dark:text-violet-400"
             >
               <path
                 strokeLinecap="round"
@@ -42,7 +42,7 @@ const TableGroup = () => {
           <input
             type="text"
             id="searchInput"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
+            className="bg-violet-50 border border-violet-300 text-violet-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full ps-10 p-2.5  dark:bg-violet-700 dark:border-violet-600 dark:placeholder-violet-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
             placeholder="Search"
           />
         </div>

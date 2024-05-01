6ce4c13df2fc7ad0cc18f1e1a77321f09d66e57e
Hanhtunaung97 <littlehanhtunaung97>
2024-05-01 22:58:30 +0630

    change file name Header to Head

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
 import Container from "./components/Container";
-import Header from "./components/header";
 import TableGroup from "./components/TableGroup";
 import CreateDrawer from "./components/CreateDrawer";
 import EditDrawer from "./components/EditDrawer";
 import { dataContext } from "./contexts/DataContext";
+import Head from "./components/Head";
+
 
 const App = () => {
   const { editDrawer } = useContext(dataContext);
   return (
     <Container>
-      <Header />
+      <Head/>
       <TableGroup />
       <CreateDrawer />
       {editDrawer && <EditDrawer />}
--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Header = () => {
+const Head = () => {
   return (
     <header className="mb-3 md:mb-5">
-      <h1 className="text-3xl font-bold font-serif mb-1">Available Play Stations</h1>
+      <h1 className="text-3xl font-bold font-serif mb-1">
+        Available Play Stations
+      </h1>
       <p className=" text-violet-500 font-semibold">React-API-Crud</p>
     </header>
   );
 };
 
-export default Header;
+export default Head;
